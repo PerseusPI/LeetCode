0a9d4f13f78c854ae8807f867ca55f6b8bff1929
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-07 15:16:24 +0100

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -4,8 +4,9 @@ Memory Usage: 15.1 MB, less than 64.24% of Python3 online submissions for Two Su
 """
 
 #LeetCode challenge TwoSum
-def twoSum(nums: list[int], target: int) -> list[int]:
-    for i in range(len(nums)):     
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:                   
-                return [i, j]
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):     
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:                   
+                    return [i, j]
