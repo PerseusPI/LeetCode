3b5a0af4de29a6742f70b7e312726a68a2e68e3d
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-07 15:13:52 +0100

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,7 +1,6 @@
 #LeetCode challenge TwoSum
-def twoSum(self, nums: list[int], target: int) -> list[int]:
+def twoSum(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):     
-        for j in range(len(nums) - 1):
-            if nums[i] + nums[j+1] == target:                  
-                if j+1 != i:  
-                    return [i, j+1]
+        for j in range(i + 1, len(nums) - 1):
+            if nums[i] + nums[j] == target:                   
+                return [i, j]
