fa1b07a3bb4b4cd27dd69af02ba1d2b831575329
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-07 15:15:55 +0100

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,3 +1,8 @@
+"""
+Runtime: 5804 ms, faster than 12.24% of Python3 online submissions for Two Sum.
+Memory Usage: 15.1 MB, less than 64.24% of Python3 online submissions for Two Sum.
+"""
+
 #LeetCode challenge TwoSum
 def twoSum(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):     
