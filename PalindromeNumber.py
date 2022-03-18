dbaf1490ad0f5cd1d0d6744ff615889c5a03d9ac
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-18 15:00:44 +0100

    Update PalindromeNumber.py

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,3 +1,8 @@
+"""
+Runtime: 77 ms, faster than 68.32% of Python3 online submissions for Palindrome Number.
+Memory Usage: 13.9 MB, less than 34.22% of Python3 online submissions for Palindrome Number.
+"""
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
